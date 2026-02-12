f80846c267c6c12d5037a077c70c0cc8c38ae4c2
Mahdi Rezaie <mahdi.rezaie6413@gmail.com>
2026-02-12 14:04:11 +0000

    Mahdi added the classes

---
+++ src/main/java/com/mahdirezaie/enterm_project/EntermProjectApplication.java
@@ -0,0 +1,11 @@
+package com.mahdirezaie.enterm_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntermProjectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EntermProjectApplication.class, args);
+    }
+}
---
+++ src/test/java/com/mahdirezaie/enterm_project/EntermProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahdirezaie.enterm_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntermProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
